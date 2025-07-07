93985852906ff71ad533e66b3b35ff712c5e2306
Maximiliano Delgado <maxidelga2@gmail.com>
2025-07-07 17:23:06 -0300

    asdf configs

--- dot_config/nvim/lua/vnext/plugins/editing.lua
+++ dot_config/nvim/lua/vnext/plugins/editing.lua
@@ -127,40 +127,6 @@ return {
   },
 
   {
-    "allaman/emoji.nvim",
-    ft = "markdown",
-    opts = {
-      enable_cmp_integration = true,
-      plugin_path = vim.fn.expand("~/workspace/github.com/allaman"),
-    },
-  },
-
-  -- emoji blink.cmp integration
-  {
-    "saghen/blink.cmp",
-    dependencies = { "allaman/emoji.nvim", "saghen/blink.compat" },
-    opts = {
-      sources = {
-        default = { "emoji" },
-        providers = {
-          emoji = {
-            name = "emoji",
-            module = "blink.compat.source",
-            -- overwrite kind of suggestion
-            transform_items = function(ctx, items)
-              local kind = require("blink.cmp.types").CompletionItemKind.Text
-              for i = 1, #items do
-                items[i].kind = kind
-              end
-              return items
-            end,
-          },
-        },
-      },
-    },
-  },
-
-  {
     "MagicDuck/grug-far.nvim",
     cmd = "GrugFar",
     opts = {},
